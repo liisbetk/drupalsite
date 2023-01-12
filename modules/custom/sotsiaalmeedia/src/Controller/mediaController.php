8022527d4b73bacf97477fae8ef2251e42c9ad6f
Liisbet <liisbetkukk@gmail.com>
2023-01-12 17:31:57 +0200

    uus

---
+++ modules/custom/sotsiaalmeedia/src/Controller/mediaController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\sotsiaalmeedia\Controller;
+
+
+class mediaController {
+
+    public function socialmediaList() {
+
+        $links = [
+            ['name' => "Facebook"],
+            ['name' => "LinkedIn"],
+            ['name' => "Twitter"],
+            ['name' => "Instagram"],
+            ['name' => "Youtube"],
+            ['name' => "Whatsapp"],
+            ['name' => "Discord"],
+            ['name' => "TikTok"],
+            ['name' => "Reddit"]
+        ];
+
+        $socialmediaLinks = "";
+        foreach ($links as $link) {
+            $socialmediaLinks .= '<li>' . $link['name'] . '</li>';
+        }
+
+        return [
+            '#type' => 'markup',
+            '#markup' => '<ol>' . $socialmediaLinks . '</ol>',
+        ];
+    }
+}
