633b7c7bc531c5d8144f9aba51b29edb0ff0ccef
Liisbet <liisbetkukk@gmail.com>
2023-01-17 11:56:07 +0200

    employeeform

---
+++ modules/custom/employee/src/Form/EmployeeForm.php
@@ -0,0 +1,90 @@
+<?php
+
+    namespace Drupal\employee\Form;
+
+    use Drupal\Core\Form\Formbase;
+    use Drupal\Core\Form\FormStateInterface;
+
+class EmployeeForm extends Formbase{
+
+    /**
+     *{@inheritDoc}
+     */
+
+     public function getFormId()
+     {
+        return 'create_employee';
+     }
+
+     /**
+      * {@inheritDoc}
+      */
+
+      public function buildForm(array $form, FormStateInterface $form_state)
+      {
+
+        $genderOpt= array(
+            '0' => 'select gender',
+            'male' => 'Male',
+            'female' => 'Female',
+        );
+        $form['name']= array(
+            '#type' => 'textfield',
+            '#title' => 'Name',
+            '#default_value' => '',
+        );
+
+
+        $form['gender'] = array(
+            '#type' => 'select',
+            '#title' => 'Gender',
+            '#options' => $genderOpt,
+        );
+
+        $form['about'] = array(
+            '#type' => 'textfield',
+            '#title' => 'job description',
+            '#dedault_value' => '',
+        );
+
+        $form['save'] = array(
+            '#type' => 'submit',
+            '#value' => 'Save',
+            '#button_type' => 'primary'
+        );
+
+        return $form;
+      }
+
+      /**
+       *{@inheritDoc}
+       */
+
+       public function validateForm(array &$form, FormStateInterface $form_state)
+       {
+
+        //Mulle tundub, et mõistlikum on kasutada buildFormis #required => true rida. Pean uurima, miks
+// näidetes pidevalt validateFormi kasutatakse.
+        $name =  $form_state->getValue('name');
+
+        if(trim($name) == '') {
+            $form_state->setErrorByName('name',$this->t('Name is required'));
+        }
+        if($form_state->getValue('gender') == '0') {
+            $form_state->setErrorByName('gender',$this->t('Select your gender'));
+        }
+        if($form_state->getValue('about') == '') {
+            $form_state->setErrorByName('about',$this->t('Please enter your job description'));
+        }
+       }
+
+      public function submitForm(array &$form, FormStateInterface $form_state)
+      {
+        $postData = $form_state->getValues();
+
+        echo '<pre>';
+        print_r($postData);
+        echo '</pre>';
+        exit;
+      }
+}
